2d9c8ac0ba5d6e7d6973d4d1a16222cc8cbe8ad7
Patrick Mabialah <pmabiala@me.com>
2020-10-11 17:28:51 +0000

    checkZero function added

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function checkZero(pObjectId){
+    var DOMobject = document.getElementById(pObjectId);
+    var tmpValue = DOMobject.value;
+
+    if (tmpValue <=0){
+        alert("Positive value required");
+        DOMobject.SetFocus();
+    }
+
+    return tmpValue;
+}
+
+function compute(){
+    var _principal = document.getElementById.value;
     
+    if ((_principal = checkZero('principal')) <= 0) return;
+    if ((_rate = checkZero('rate')) <=0) return;
+    if ((_year = checkZero('year')) <=0) return;
+    alert(_principal);
+
+    var _interest = _principal * _rate * _years / 100;
+
+    document.getElementById('result').innerHTML = _interest;
 }
         
