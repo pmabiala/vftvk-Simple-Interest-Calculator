d45b79bae463c6fa49005c43915bd12f491b00db
Patrick Mabialah <pmabiala@me.com>
2020-10-13 19:50:10 +0000

    output text

--- script.js
+++ script.js
@@ -11,14 +11,19 @@ function checkZero(pObjectId){
 }
 
 function compute(){
-    var _principal = document.getElementById.value;
-    
+    /* Retrieve forms values */
     if ((_principal = checkZero('principal')) <= 0) return;
     if ((_rate = checkZero('rate')) <=0) return;
     if ((_years = checkZero('years')) <=0) return;
 
+    /* Compute interest */
     var _interest = _principal * _rate * _years / 100;
 
-    document.getElementById('result').innerHTML = _interest;
+    /* Build output text */
+    var output = "If you deposit <strong>" + _principal + "</strong>,<br>";
+    output += "at an interest rate of <strong>" + _rate + "</strong>.<br>";
+    output += "You will receive an amount of <strong>" + _interest + "</strong>, <br>"
+    output += "in the year " + (Number.parseInt(new Date().getFullYear()) + Number.parseInt(_years));
+    document.getElementById('result').innerHTML = output;
 }
         
